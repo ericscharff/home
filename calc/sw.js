20d8f5cc31b1a8ba9deb4d6dea6d42708f92389b
Eric Scharff <eric.scharff@gmail.com>
2026-02-09 18:54:19 -0700

    Clean up old cache entries

--- calc/sw.js
+++ calc/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'calc-v2';
+const CACHE_NAME = 'calc-v3';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -12,6 +12,21 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Deleting old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
